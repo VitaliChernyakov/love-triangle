af0340e7c7c02cd507bab7f8e72cc964d66ef514
vitali.chernyakov@gmail.com <chernyakov@irc.beltelecom.by>
2019-02-20 10:26:07 +0300

    First

--- src/index.js
+++ src/index.js
@@ -3,5 +3,6 @@
  * @returns number of love triangles
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
-  // your implementation123
+  // your implementation
+  return 1;
 };
